248e731900abf29839e782053425c54e6b8f4497
Akanibekuly <a.kanibekuly@gmail.com>
2021-05-14 15:53:20 +0600

    feat: shep request and response

---
+++ models/models.document.request.go
@@ -0,0 +1,23 @@
+package models
+
+type DocumentRequest struct {
+	Iin      string
+	Services map[string]ServiceDTO
+	DocumentTypeDto `json:"documentType"`
+}
+
+type ServiceDTO struct {
+	ID        float64
+	Code      string
+	Name      string
+	Url       string
+	ServiceId string
+}
+
+type DocumentTypeDto struct {
+	ID     float64
+	Code   string
+	NameRu string
+	NameKk string
+	NameEn string
+}
---
+++ models/models.envelope.request.go
@@ -0,0 +1,64 @@
+package models
+
+import "encoding/xml"
+
+const (
+	ENVELOPE           = "Envelope"
+	ENVELOP_SCHEMA     = "http://schemas.xmlsoap.org/soap/envelope/"
+	SEND_MESSAGE_XMLNS = "http://bip.bee.kz/SyncChannel/v10/Types"
+)
+
+type Data struct {
+	Text     string `xml:",chardata"`
+	Ns6      string `xml:"xmlns:ns6,attr"`
+	Xsi      string `xml:"xmlns:xsi,attr"`
+	Type     string `xml:"xsi:type,attr"`
+	Iin      string `xml:"iin"`
+	Login    string `xml:"login"`
+	Password string `xml:"password"`
+}
+
+type RequestData struct {
+	Text string `xml:",chardata"`
+	Data *Data  `xml:"data"`
+}
+
+type SenderCred struct {
+	Text     string `xml:",chardata"`
+	SenderId string `xml:"senderId"`
+	Password string `xml:"password"`
+}
+
+type RequestInfo struct {
+	Text        string      `xml:",chardata"`
+	MessageId   string      `xml:"messageId"`
+	ServiceId   string      `xml:"serviceId"`
+	MessageDate string      `xml:"messageDate"`
+	Sender      *SenderCred `xml:"sender"`
+}
+
+type Request struct {
+	Text    string       `xml:",chardata"`
+	ReqInfo *RequestInfo `xml:"requestInfo"`
+	ReqData *RequestData `xml:"requestData"`
+}
+
+type SendMessageRequest struct {
+	Text string   `xml:",chardata"`
+	Ns2  string   `xml:"xmlns:ns2,attr"`
+	Ns3  string   `xml:"xmlns:ns3,attr"`
+	Ns4  string   `xml:"xmlns:ns4,attr"`
+	Req  *Request `xml:"request"`
+}
+
+type BodyRequest struct {
+	Text        string              `xml:",chardata"`
+	SendMessage *SendMessageRequest `xml:"ns2:SendMessage"`
+}
+
+type EnvelopeRequest struct {
+	XMLName xml.Name     `xml:"soap:Envelope"`
+	Text    string       `xml:",chardata"`
+	Xmlns   string       `xml:"xmlns:soap,attr"`
+	Body    *BodyRequest `xml:"soap:Body"`
+}
