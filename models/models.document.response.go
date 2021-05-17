ed68ac60fcd2ae3557dd7cc1d469421b2ea3c2d6
Akanibekuly <a.kanibekuly@gmail.com>
2021-05-17 15:03:05 +0600

    feat: new struct for documnets response and configs for pass

---
+++ models/models.document.response.go
@@ -0,0 +1,69 @@
+package models
+
+type DocResponse struct {
+	Found     bool
+	Active    bool
+	Documents []*DocumentResponseItem
+}
+
+type DocumentResponseItem struct {
+	Valid            bool
+	Number           string
+	Title            string
+	TitleRu          string
+	TitleKk          string
+	IssuedDate       string
+	ExpirationDate   string
+	SerializableData string
+	StorageID        string
+}
+
+type IssuedDigiDoc struct {
+	common DocCommon
+	domain PcrCertificate
+}
+
+type DocCommon struct {
+	DocOwner          DocPerson
+	DocType           DocType
+	DocNumber         string
+	DocIssuer         string
+	DocIssuedDate     string
+	DocExpirationDate string
+	DocUri            string
+}
+
+type DocPerson struct {
+	Iin       string
+	FirstName string
+	LastName  string
+}
+
+type DocType struct {
+	Code string
+	I18Text
+}
+
+type I18Text struct {
+	NameKk string
+	NameRu string
+	NameEn string
+}
+
+type PcrCertificate struct {
+	Key              string
+	FirstName        string
+	LastName         string
+	MiddleName       string
+	Iin              string
+	Gender           string
+	isResident       string
+	Adress           string
+	Birthday         string
+	PlaceOfStudy     string
+	Phone            string
+	HasSymptomsCOVID string
+	ProtovolDate     string
+	CreateAt         string
+	ResearchResults  string
+}
